1a6ddd08eceb8c690c2a8828746dc1991f35e50b
Bart Kang <bart.kang.0719@gmail.com>
2025-01-29 13:22:03 -0800

    demo loading

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,3 +1,14 @@
 export default function Contact() {
-  return <div>contact page</div>;
+  const result: Promise<string> = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Hi contact page can be rendered");
+    }, 2000);
+  });
+
+  return (
+    <div>
+      <h1>Contact Page</h1>
+      <p>{result}</p>
+    </div>
+  );
 }
