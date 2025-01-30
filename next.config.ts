bb9d9976182fcfdc408c519ca96cd54cfc101ae7
Bart Kang <bart.kang.0719@gmail.com>
2025-01-29 22:47:50 -0800

    fix image hostname

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,18 @@
 import type { NextConfig } from "next";
 
-// https://api.thecatapi.com/v1/images/search?limit=10
+// https://api.thecatapi.com/v1/images/search?limit=10 => wrong url...
+// has to be image url
+// https://cdn2.thecatapi.com/images/b9j.jpg <= correct one...
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
     remotePatterns: [
       {
-        hostname: "*"
+        protocol: "https",
+        hostname: "cdn2.thecatapi.com",
+        port: "",
+        pathname: "/images/**",
+        search: ""
       }
     ]
   }
